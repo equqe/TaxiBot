13f10358e3306304a4b35a10e34e67df73bec2fb
gagtain <r.tis@list.ru>
2023-09-22 21:16:16 +0300

    general

--- core/core/urls.py
+++ core/core/urls.py
@@ -6,11 +6,6 @@ from django.urls import include, path, reverse
 from django.views.generic import RedirectView
 
 
-def error_404_view(request: HttpRequest, exception=None):
-    return HttpResponse(
-        '<h1>Страница не найдена</h1><br><a href="%s">Кабинет</a>'
-        % reverse("cabinet:index")
-    )
 
 
 def error_403_view(request: HttpRequest, exception=None):
@@ -32,5 +27,4 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
-handler404 = error_404_view
 handler403 = error_403_view
